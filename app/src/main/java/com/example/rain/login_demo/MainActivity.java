059bf3902e92d2a6d75179842b8f4c2bffc159b6
rainf0rest <405763281@qq.com>
2017-05-01 14:22:02 +0800

    first commit

---
+++ app/src/main/java/com/example/rain/login_demo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.rain.login_demo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button loginbtn, signinbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginbtn = (Button) findViewById(R.id.btn_login);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SigninActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/rain/login_demo/SigninActivity.java
@@ -0,0 +1,16 @@
+package com.example.rain.login_demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by rain on 2017/5/1.
+ */
+
+public class SigninActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_layout);
+    }
+}
